4828c9420c3325cb58ddf20b37ef6c708399049d
crackheadakira <lasn.mine@gmail.com>
2022-07-14 19:22:22 +0200

    Add user configuration to .json file

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const socialLink = document.querySelector('#socialLink')
+const socialLink = document.querySelector('#socialLink');
+const profile = document.querySelector('#profile');
+const title = document.querySelector('#title');
 
-getSocials().then(data => {
-    for(social in data){
-        let items = Object.values(data[social]);
-        if(items[2] <= 0){ break }
+getJSON().then(data => {
+    profile.src = data.user.profile;
+    title.textContent = data.user.name;
+    for (social in data.socials) {
+        let items = Object.values(data.socials[social]);
+        if (items[2] <= 0) { break }
         const a = document.createElement("a");
         const img = document.createElement("img");
-        a.href=items[1];
-        a.target="_blank";
-        img.src=items[2];
-        img.alt=items[0];
+        a.href = items[1];
+        a.target = "_blank";
+        img.src = items[2];
+        img.alt = items[0];
         img.classList.add("socialLink");
         a.appendChild(img);
         socialLink.appendChild(a);
     }
 })
 
-async function getSocials(){
+async function getJSON() {
     const requestURL = './socials.json';
     const request = new Request(requestURL);
     const response = await fetch(request);
